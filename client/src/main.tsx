e60f6a1f6aac65eb1e5ff75624620cc20e5f8bd0
Benjamin Lu <b.lu789321@gmail.com>
2024-09-17 17:08:18 -0700

    added browser router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,19 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+import { BrowserRouter } from "react-router-dom"; // Add this line
 
-// For some reaosn importing enviroment variable isn't working
-//FIX: env local files should be located in the root directory. This is because vite is designed to look into the root of the application
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+// Set the basename to your repository name
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <BrowserRouter basename="/financial-tracker-MERN">
+        <App />
+      </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
 );
