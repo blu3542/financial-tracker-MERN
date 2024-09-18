e4a849a0eb355ac452b2958095e5b3602ef19910
Benjamin Lu <b.lu789321@gmail.com>
2024-09-17 22:19:27 -0700

    final

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Dashboard } from "./pages/dashboard";
 import { Auth } from "./pages/auth";
 import { FinancialRecordsProvider } from "./contexts/financial-record-context";
@@ -7,28 +7,25 @@ import { SignedIn, UserButton } from "@clerk/clerk-react";
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <div className="navbar">
-          <Link to="/"> Dashboard </Link>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </div>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <FinancialRecordsProvider>
-                {" "}
-                <Dashboard />{" "}
-              </FinancialRecordsProvider>
-            }
-          />
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
+    <div className="app-container">
+      <div className="navbar">
+        <Link to="/"> Dashboard </Link>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
-    </Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <FinancialRecordsProvider>
+              <Dashboard />
+            </FinancialRecordsProvider>
+          }
+        />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </div>
   );
 }
 
